9673b5ac70d9fd90dab2a6da89637e12dbe9bb8c
Luis Miguel Martín <luimargon3@alum.us.es>
2023-02-27 20:17:24 +0100

    Servidor casi funcionando

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Tener una ruta dinámica “/cool” que muestre una cara ascii tal como se vió en el L05.
 var express = require("express");
 var cool = require("cool-ascii-faces");
 
@@ -11,4 +12,25 @@ app.get("/cool", (request,response) => {
 
 app.listen(port,()=>{
     console.log(`Server ready in port ${port}`);
+});
+
+// ruta de /samples/index-LMG.js
+var { luis } = require("./index-LMG.js");
+app.get("/samples/index-LMG", (request,response) => {
+    response.send(luis());
+    console.log("New request"); //console.log en el servidor    
+});
+
+// ruta de /samples/index-JLB.js
+var { jose } = require("./index-JLB.js");
+app.get("/samples/index-JLB", (request,response) => {
+    response.send(jose());
+    console.log("New request"); //console.log en el servidor    
+});
+
+// ruta de /samples/index-IFR.js
+var { irene } = require("./index-IFR.js");
+app.get("/samples/index-IFR", (request,response) => {
+    response.send(irene());
+    console.log("New request"); //console.log en el servidor    
 });
