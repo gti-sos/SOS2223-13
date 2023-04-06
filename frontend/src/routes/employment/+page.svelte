20e63ce54e5ed3ae23472c32d42e4db6dae0aef4
joslopber <joslopber@alum.us.es>
2023-04-06 14:39:56 +0200

    POST

--- frontend/src/routes/employment/+page.svelte
+++ frontend/src/routes/employment/+page.svelte
@@ -16,7 +16,12 @@
             
         let employments = [];
         let newEmploymentYear = 'year';
+        let newEmploymentPeriod = 'period';
+        let newEmploymentDate = 'date';
         let newEmploymentRegion = 'region';
+        let newEmploymentEmployedPerson = 'employed person';
+        let newEmploymentInactivePerson = 'inactive person';
+        let newEmploymentUnemployedPerson = 'unemployed person';
     
         let result = "";
         let resultStatus = "";
@@ -41,17 +46,37 @@
         async function createEmployment (){
             resultStatus = result = "";
             const res = await fetch(API, {
-                method: 'POST'      //
+                method: 'POST',
+                headers:{
+                    "Content-Type" : "application/json"
+                },
+                body:JSON.stringify({
+                    year: newEmploymentYear,
+                    period: newEmploymentPeriod,
+                    date: newEmploymentDate,
+                    region: newEmploymentRegion,
+                    employed_person: newEmploymentEmployedPerson,
+                    inactive_person: newEmploymentInactivePerson,
+                    unemployed_person: newEmploymentUnemployedPerson
+                })     
             });
-            try{
-                const data = await res.json();
-                result = JSON.stringify(data,null,2);
-                employments = data;
-            }catch(error){
-                console.log(`Error parsing result: ${error}`);
-            }
             const status = await res.status;
-            resultStatus = status;	
+            resultStatus = status;
+            if(status==201){
+                getEmployments ();
+            }	
+        }
+
+        async function deleteEmploymentAll () {
+            resultStatus = result = "";
+            const res = await fetch(API, {
+                method: 'DELETE'
+            });
+            const status = await res.status;
+            resultStatus = status;
+            if(status==200){
+                await getEmployments ();
+            }		
         }
     
     
@@ -64,9 +89,9 @@
     <Table>
         <thead>
           <tr>
-            <th>#</th>
             <th>Year</th>
             <th>Period</th>
+            <th>Date</th>
             <th>Region</th>
             <th>Employed person</th>
             <th>Inactive person</th>
@@ -76,22 +101,30 @@
         </thead>
         <tbody>
             <td><input bind:value={newEmploymentYear}></td>
+            <td><input bind:value={newEmploymentPeriod}></td>
+            <td><input bind:value={newEmploymentDate}></td>
             <td><input bind:value={newEmploymentRegion}></td>
+            <td><input bind:value={newEmploymentEmployedPerson}></td>
+            <td><input bind:value={newEmploymentInactivePerson}></td>
+            <td><input bind:value={newEmploymentUnemployedPerson}></td>
             <td><Button on:click={createEmployment}>Create</Button></td>
            
 
         {#each employments as employment}
           <tr>
-            <th scope="row">1</th>
             <td>{employment.year}</td>
             <td>{employment.period}</td>
+            <td>{employment.date}</td>
             <td>{employment.region}</td>
             <td>{employment.employed_person}</td>
             <td>{employment.inactive_person}</td>
             <td>{employment.unemployed_person}</td>
             <td>&nbsp</td>
+           
           </tr>
         {/each}
+
+        <td><Button on:click={deleteEmploymentAll}>Delete all</Button></td>
           
         </tbody>
     </Table>
