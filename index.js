bea35dda8ae3f9308e779a62344930d98a95293a
joslopber <joslopber@alum.us.es>
2023-04-07 20:34:50 +0200

    version2

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ import cors from "cors";
 //var modulo_luis = require('./Modularizado/api');
 //var modulo_irene = require('./Modularizado/apiirene');
 import {loadBackendJLB} from './Modularizado/apiJLB.js';
+import {loadBackendJLBV2} from './Modularizado/v2/apiJLBV2.js';
 import {loadBackendLMMG} from './Modularizado/api.js';
 //const { application } = require("express");
 import {handler} from "./frontend/build/handler.js";
@@ -53,6 +54,7 @@ const BASE_API_URL = "/api/v1";
 
 
 loadBackendJLB(app);
+loadBackendJLBV2(app);
 loadBackendLMMG(app);
 
 app.use(handler);
