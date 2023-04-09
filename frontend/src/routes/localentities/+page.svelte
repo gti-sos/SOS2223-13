2f533ab25bbd275c5ad09e7994529e25945d8777
ireferrom <ireferrom@alum.us.es>
2023-04-09 13:24:48 +0200

    cambios

--- frontend/src/routes/localentities/+page.svelte
+++ frontend/src/routes/localentities/+page.svelte
@@ -9,8 +9,9 @@
             getLocalentities();
         });
         
-        let API = '/api/v1/localentities-stats';
-        
+        let API = '/api/v2/localentities-stats';
+        let aviso = "";
+
         if(dev)
             API = 'http://localhost:8080'+API
             
@@ -44,33 +45,79 @@
             resultStatus = status;	
         }
 
-
+        let insertedData = [];
         async function createLocalentities (){
             resultStatus = result = "";
+            const newLocalentities = {
+                province: newLocalentitiesProvince,
+                landline: newLocalentitiesLandline,
+                first_name: newLocalentitiesFirstName,
+                second_name: newLocalentitiesSecondName,
+                president_appointment_date: parseInt(newLocalentitiesPresidentAppointmentDate),
+                surface_extension: newLocalentitiesSurfaceExtension,
+                population: newLocalentitiesPopulation,
+                expense: newLocalentitiesExpense,
+                income: newLocalentitiesIncome
+            };
+
+            const existingData = insertedData.find(data => 
+                data.province === newLocalentitiesProvince && data.landline === newLocalentitiesLandline && data.first_name === newLocalentitiesFirstName
+                    && data.second_name === newLocalentitiesSecondName && data.president_appointment_date === newLocalentitiesPresidentAppointmentDate && data.surface_extension === newLocalentitiesSurfaceExtension && 
+                    data.population === newLocalentitiesPopulation && data.expense === newLocalentitiesExpense && data.income === newLocalentitiesIncome
+            );
+
+            if (existingData) {
+                aviso = "Ya existe ese dato";
+                return;
+            }
+
             const res = await fetch(API, {
                 method: 'POST',
                 headers:{
                     "Content-Type" : "application/json"
                 },
-                body:JSON.stringify({
-                    province: newLocalentitiesProvince,
-                    landline: newLocalentitiesLandline,
-                    first_name: newLocalentitiesFirstName,
-                    second_name: newLocalentitiesSecondName,
-                    president_appointment_date: newLocalentitiesPresidentAppointmentDate,
-                    surface_extension: newLocalentitiesSurfaceExtension,
-                    population: newLocalentitiesPopulation,
-                    expense: newLocalentitiesExpense,
-                    income: newLocalentitiesIncome
+                body: JSON.stringify(
+                    newLocalentities
+                )     
+            });	
 
-                })     
+            const status = await res.status;
+            resultStatus = status;
+            if (status == 201) {
+                getLocalentities();
+                aviso = "Se ha creado el nuevo dato introducido";
+                insertedData.push(newLocalentities);
+            } else if (status == 409) {
+                aviso = "El dato introducido ya existe";
+                getLocalentities();
+            } else if (status == 400) {
+                aviso = "Las propiedades introducidas no tienen un formato correcto";
+                getLocalentities();
+            } else {
+                aviso = "No se ha podido crear el dato introducido";
+                getLocalentities();
+            }
+        }   
+
+        async function deleteLocalentities(localentitiesProvince, localentitiesPresidentAppointmentDate){
+            resultStatus = result = "";
+            const res = await fetch(API+"/"+localentitiesProvince+"/"+localentitiesPresidentAppointmentDate, {
+                method: "DELETE"
             });
             const status = await res.status;
             resultStatus = status;
-            if(status==201){
+            if(status==200){
                 getLocalentities ();
-            }	
+                aviso = "Recurso borrado";
+            }else if(status==500){
+                aviso = "Error cliente";
+            }else if(status==404){
+                getLocalentities ();
+                aviso = "No se ha encontrado ese recurso";
+            }
         }
+
+
 
         async function deleteLocalentitiesAll () {
             resultStatus = result = "";
@@ -79,18 +126,24 @@
             });
             const status = await res.status;
             resultStatus = status;
-            if(status==200){
-                await getLocalentities ();
-            }		
+            if(status==200 || status == 204){
+                await getLocalentities();
+                aviso = "Se han borrado correctamente los datos";
+            }else{
+                aviso = "No se han podido borrar los datos";
+            }			
         }
     
     
     
     
         
-    
     </script>
-    <h1>Local Entities</h1>
+    <h1 style="text-align: center; font-family:'Times New Roman', Times, serif; font-size: 60px;">Datos Local Entities</h1>
+    {#if aviso !=""}
+    <h2 style="color: red; text-align: center; font-family:Arial, Helvetica, sans-serif">{aviso}</h2>
+    {/if}
+
     <Table>
         <thead>
           <tr>
@@ -123,6 +176,8 @@
         {#each localentities as loen}
           <tr>
             <td>{loen.province}</td>
+            <td><a href="/localentities-stats/{loen.province}/{loen.president_appointment_date}">{loen.province}</a></td>
+
             <td>{loen.landline}</td>
             <td>{loen.first_name}</td>
             <td>{loen.second_name}</td>
@@ -131,7 +186,8 @@
             <td>{loen.population}</td>
             <td>{loen.expense}</td>
             <td>{loen.income}</td>
-            <td>&nbsp</td>
+
+            <td><Button on:click={deleteLocalentities(loen.province, loen.president_appointment_date)}>Borrar</Button></td>
            
           </tr>
         {/each}
@@ -143,11 +199,12 @@
 
 
     {#if resultStatus != ""}
-        <p>
-            Result:
-        </p>
-        <pre>
-{resultStatus}
+    <p>
+        <strong>Número de datos: {localentities.length}</strong>
+    </p>
+    <strong>Result:</strong>
+    <pre>
+{"Código de estado: "+resultStatus}
 {result}
-        </pre>
-    {/if}
+    </pre>
+{/if}
---
+++ frontend/src/routes/localentities/[province]/[president_appointment_date]/+page.svelte
@@ -0,0 +1,148 @@
+<script>
+    // @ts-nocheck
+    
+        import { onMount } from 'svelte';
+        import { dev } from '$app/localentities';
+        import { Button,Table } from 'sveltestrap';
+        import { page } from "$app/stores";
+
+        onMount(async () => {
+            getLocalentities_dato();
+        });
+        
+        let aviso = "";
+        let province = $page.params.province;
+        let president_appointment_date = $page.params.president_appointment_date;
+
+        let API = "/api/v2/localentities-stats/"+province+ "/" +president_appointment_date;
+
+        if(dev)
+            API = 'http://localhost:8080'+API
+            
+        let updateLocalentitiesProvince = province;
+        let updateLocalentitiesLandline = 'landline';
+        let updateLocalentitiesFirstName = 'first_name';
+        let updateLocalentitiesSecondName = 'second_name';
+        let updateLocalentitiesPresidentAppointmentDate = president_appointment_date;
+        let updateLocalentitiesSurfaceExtension = 'surface_extension';
+        let updateLocalentitiesPopulation = 'population';
+        let updateLocalentitiesExpense = 'expense';
+        let updateLocalentitiesIncome = 'income';
+
+        let result = "";
+        let resultStatus = "";
+    
+        async function getLocalentities_dato () {
+            resultStatus = result = "";
+            const res = await fetch(API, {
+                method: 'GET'
+            });
+            try{
+                const data = await res.json();
+                result = JSON.stringify(data,null,2);
+                updateLocalentitiesProvince = data.province;
+                updateLocalentitiesLandline = data.landline;
+                updateLocalentitiesFirstName = data.first_name;
+                updateLocalentitiesSecondName = data.second_name;
+                updateLocalentitiesPresidentAppointmentDate = data.president_appointment_date;
+                updateLocalentitiesSurfaceExtension = data.surface_extension;
+                updateLocalentitiesPopulation = data.population;
+                updateLocalentitiesExpense = data.expense;
+                updateLocalentitiesIncome = data.income;
+
+            }catch(error){
+                console.log(`Error parsing result: ${error}`);
+            }
+
+            const status = await res.status;
+            resultStatus = status;	
+            if(status==404){
+                aviso = `La ruta solicitada "${province}/${president_appointment_date}" no existe`;
+            }
+        }
+
+        async function updateLocalentities (){
+            resultStatus = result = "";
+            const res = await fetch(API, {
+                method: "PUT",
+                headers:{
+                    "Content-Type": "application/json"
+                },
+                body:JSON.stringify({
+                    province: updateLocalentitiesProvince,
+                    landline: updateLocalentitiesLandline,
+                    first_name: updateLocalentitiesFirstName,
+                    second_name: updateLocalentitiesSecondName,
+                    president_appointment_date: updateLocalentitiesPresidentAppointmentDate,
+                    surface_extension: updateLocalentitiesSurfaceExtension,
+                    population: updateLocalentitiesPopulation,
+                    expense: updateLocalentitiesExpense,
+                    income: updateLocalentitiesIncome
+
+                })
+            });
+           
+            const status = await res.status;
+            resultStatus = status;
+            if(status==200){
+                getLocalentities_dato();
+                aviso = "Se ha actualizado el dato";
+            }else if(status==400){ 
+                aviso = "Los datos introducidos no son válidos";
+            }else{
+                aviso = "No se ha podido actualizar el dato";
+            }       
+        }
+    
+    
+        
+    
+    </script>
+
+    <h1 style="text-align: center; font-family:'Times New Roman', Times, serif; font-size: 60px;">Local Entities Cambios</h1>
+      
+    {#if aviso !=""}
+        <h2 style="color: red; text-align: center; font-family:Arial, Helvetica, sans-serif">{aviso}</h2>
+    {/if}
+
+    <Table>
+        <thead>
+          <tr>
+            <th>Provincia</th>
+            <th>Teléfono</th>
+            <th>Nombre</th>
+            <th>Apellido</th>
+            <th>Fecha nombramiento presidente</th>
+            <th>Superficie</th>
+            <th>Poblacion</th>
+            <th>Gastos</th>
+            <th>Ingresos</th>
+            <th>Acción</th>
+          </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>{updateLocalentitiesProvince}</td>
+                <td><input bind:value={updateLocalentitiesLandline}></td>
+                <td><input bind:value={updateLocalentitiesFirstName}></td>
+                <td><input bind:value={updateLocalentitiesSecondName}></td>
+                <td>{updateLocalentitiesPresidentAppointmentDate}</td>
+                <td><input bind:value={updateLocalentitiesSurfaceExtension}></td>
+                <td><input bind:value={updateLocalentitiesPopulation}></td>
+                <td><input bind:value={updateLocalentitiesExpense}></td>
+                <td><input bind:value={updateLocalentitiesIncome}></td>
+
+                <td><Button on:click={updateLocalentities}>Actualiza</Button></td>
+            </tr>
+        </tbody>
+    </Table>
+
+
+    
+    {#if resultStatus != ""}
+            <strong>Result:</strong>
+        <pre>
+    {"Código de estado: "+resultStatus}    
+{result}
+        </pre>
+    {/if}
