2df54f66581198483a4ec8915832e0ff23d0b018
joslopber <joslopber@alum.us.es>
2023-04-10 17:18:59 +0200

    correccion

--- Modularizado/v2/apiJLBV2.js
+++ Modularizado/v2/apiJLBV2.js
@@ -403,6 +403,8 @@ app.put('/api/v1/employment-stats/:city/:year', (req, res) => {
                 });
   if (!filteredList || city!==citybody || year!==yearbody) {
     return res.status(400).json('Estadística errónea');
+  }else if(filteredList.length != 7){
+    return res.status(400).json('Estadística errónea');
   }else{
     filteredList.period = req.body.period || filteredList.period;
     filteredList.date = req.body.date || filteredList.date;
