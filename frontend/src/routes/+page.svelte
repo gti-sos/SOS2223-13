324c97be1cfee779c58a9966d8e35119ac0ce179
Luis Miguel Martín <luimargon3@alum.us.es>
2023-04-10 14:12:22 +0200

    Home

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,79 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<body>
+    <h1>BIENVENIDO a SOS2223-13</h1>
+    <ul dir="auto">
+        <p></p>
+        <li><strong>Grupo:</strong>
+        <ul dir="auto">
+        <li><a href="https://github.com/luismg01">Luis Miguel Martín González</a> (fuente de datos: Evolution)</li>
+        <li><a href="https://github.com/joslopber">José López Berro</a> (fuente de datos: Employment)</li>
+        <li><a href="https://github.com/irenefernandezromero">Irene Fernández Romero</a> (fuente de datos: Locantities)</li>
+        </ul>
+        </li>
+        <p></p>
+        <li><strong>Descripción del Proyecto:</strong> Nuestras fuentes de información están dirigidas a analizar la relación entre la evolución de la población andaluza, el número de parados y diferentes datos de los entes locales votados en las distintas provincias.
+        </li>
+        <p></p>
+        <li><strong>Repositorio:</strong> <a href="https://github.com/gti-sos/SOS2223-13">gti-sos/SOS2223-13</a></li>
+        <p></p>
+        <li><strong>URL:</strong> <a href="https://sos2223-13.appspot.com">https://sos2223-13.appspot.com</a></li>
+        <p></p>
+        <li><strong>APIs-V1:</strong>
+        <ul dir="auto">
+        <li><a href="https://sos2223-13.appspot.com/api/v1/evolution-stats" rel="nofollow">https://sos2223-13.appspot.com/api/v1/evolution-stats</a> (desarrollado por <a href="https://github.com/luismg01">Luis Miguel Martín González</a>)</li>
+        <li><a href="https://sos2223-13.appspot.com/api/v1/employment-stats" rel="nofollow">https://SOS2223-13.appspot.com/api/v1/employments-stats</a> (desarrollado por <a href="https://github.com/joslopber">Jose López Berro</a>)</li>
+        <li><a href="https://sos2223-13.appspot.com/api/v1/localentities-stats" rel="nofollow">https://sos2223-13.appspot.com/api/v1/locantities-stats</a> (desarrollado por <a href="https://github.com/irenefernandezromero">Irene Fernández Romero</a>)</li>
+        </ul></li>
+        <p></p>
+        <li><strong>Documentación-V1:</strong>
+        <ul dir="auto">
+        <li><a href="https://sos2223-13.appspot.com/api/v1/evolution-stats/docs" rel="nofollow">https://sos2223-13.appspot.com/api/v1/evolution-stats/docs</a> (desarrollado por <a href="https://github.com/luismg01">Luis Miguel Martín González</a>)</li>
+        <li><a href="https://sos2223-13.appspot.com/api/v1/employment-stats/docs" rel="nofollow">https://SOS2223-13.appspot.com/api/v1/employments-stats/docs</a> (desarrollado por <a href="https://github.com/joslopber">Jose López Berro</a>)</li>
+        <li><a href="https://sos2223-13.appspot.com/api/v1/localentities-stats/docs" rel="nofollow">https://sos2223-13.appspot.com/api/v1/locantities-stats/docs</a> (desarrollado por <a href="https://github.com/irenefernandezromero">Irene Fernández Romero</a>)</li>
+        </ul></li>
+        <p></p>
+        <li><strong>APIs-V2:</strong>
+        <ul dir="auto">
+        <li><a href="https://sos2223-13.appspot.com/api/v2/evolution" rel="nofollow">https://sos2223-13.appspot.com/api/v2/evolution-stats</a> (desarrollado por <a href="https://github.com/luismg01">Luis Miguel Martín González</a>)</li>
+        <li><a href="https://sos2223-13.appspot.com/api/v2/employment-stats" rel="nofollow">https://SOS2223-13.appspot.com/api/v2/employments-stats</a> (desarrollado por <a href="https://github.com/joslopber">Jose López Berro</a>)</li>
+        <li><a href="https://sos2223-13.appspot.com/api/v2/localentities-stats" rel="nofollow">https://sos2223-13.appspot.com/api/v2/locantities-stats</a> (desarrollado por <a href="https://github.com/irenefernandezromero">Irene Fernández Romero</a>)</li>
+        </ul></li>
+        <p></p>
+        <li><strong>Documentación-V2:</strong>
+        <ul dir="auto">
+        <li><a href="https://sos2223-13.appspot.com/api/v1/evolution/docs" rel="nofollow">https://sos2223-13.appspot.com/api/v1/evolution/docs</a> (desarrollado por <a href="https://github.com/luismg01">Luis Miguel Martín González</a>)</li>
+        <li><a href="https://sos2223-13.appspot.com/api/v1/employment-stats/docs" rel="nofollow">https://SOS2223-13.appspot.com/api/v1/employments-stats/docs</a> (desarrollado por <a href="https://github.com/joslopber">Jose López Berro</a>)</li>
+        <li><a href="https://sos2223-13.appspot.com/api/v1/localentities-stats/docs" rel="nofollow">https://sos2223-13.appspot.com/api/v1/locantities-stats/docs</a> (desarrollado por <a href="https://github.com/irenefernandezromero">Irene Fernández Romero</a>)</li>
+        </ul></li>
+        <p></p>
+        <li><strong>Frontends:</strong>
+        <ul dir="auto">
+        <li><a href="https://sos2223-13.appspot.com/evolution" rel="nofollow">https://sos2223-13.appspot.com/evolution</a> (desarrollado por <a href="https://github.com/luismg01">Luis Miguel Martín González</a>)</li>
+        <li><a href="https://sos2223-13.appspot.com/employment-stats" rel="nofollow">https://SOS2223-13.appspot.com/employments-stats</a> (desarrollado por <a href="https://github.com/joslopber">Jose López Berro</a>)</li>
+        <li><a href="https://sos2223-13.appspot.com/localentities-stats" rel="nofollow">https://sos2223-13.appspot.com/locantities-stats</a> (desarrollado por <a href="https://github.com/irenefernandezromero">Irene Fernández Romero</a>)</li>
+        </ul></li>
+    </ul>
+</body>
+
+<style>
+    h1{
+        color: blue;
+        font-family:'Times New Roman', Times, serif;
+        font-size: 40px;
+        text-align: center;
+        text-decoration: underline;
+    }
+    
+    strong{
+        font-family:'Times New Roman', Times, serif;
+        font-size: 20px;
+    }
+
+    li{
+        font-family: 'Times New Roman', Times, serif;
+        font-size: 17px;
+    }
+
+    a{
+        text-decoration: none;
+    }
+</style>
