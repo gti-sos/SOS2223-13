69a39e764bbbf36060ba80abb01248337e818765
Luis Miguel Martín <luimargon3@alum.us.es>
2023-04-10 14:24:59 +0200

    final

--- frontend/src/routes/Header.svelte
+++ frontend/src/routes/Header.svelte
@@ -1,6 +1,6 @@
 <nav>
-    <a href="/">Home</a>
-    <a href="/employment">Employment</a>
-    <a href="/evolution">Evolution</a>
-    <a href="/localentities">Local entities</a>
+    <a href="/">Menú</a>
+    <a href="/employment">Empleados</a>
+    <a href="/evolution">Evolución</a>
+    <a href="/localentities">Entidades locales</a>
 </nav>
