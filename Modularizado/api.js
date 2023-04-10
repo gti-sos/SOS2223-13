616074576df5353b754cf8756768dcf26cbd46e5
Luis Miguel Martín <luimargon3@alum.us.es>
2023-04-10 13:30:04 +0200

    uno mas

--- Modularizado/api.js
+++ Modularizado/api.js
@@ -443,7 +443,7 @@ app.get('/api/v1/evolution-stats/:territory/:year', (req, res) => {
     if(req.query.limit != undefined || req.query.offset != undefined){
       filteredList = pagination(req,filteredList);
   }
-    res.status(200).json(filteredList);
+    res.status(200).json(filteredList[0]);
   } else {
     res.status(404).json('La ruta solicitada no existe');
   }
