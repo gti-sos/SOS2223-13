a63c7fc4fc8feae896b85f9812f122cb944a06be
Luis Miguel Martín <luimargon3@alum.us.es>
2023-04-23 19:20:08 +0200

    hay que seguir luchando porque esto no va

--- frontend/src/routes/graficoLMMG/fusionchart.svelte
+++ frontend/src/routes/graficoLMMG/fusionchart.svelte
@@ -4,14 +4,19 @@
     <script type="text/javascript" src="https://cdn.fusioncharts.com/fusioncharts/latest/fusioncharts.js"></script>
     <!-- Step 2 - Include the fusion theme -->
     <script type="text/javascript" src="https://cdn.fusioncharts.com/fusioncharts/latest/themes/fusioncharts.theme.fusion.js"></script>
+    
 </head>
+
 <script>
     // @ts-nocheck
     import {onMount} from "svelte";
     const delay = ms => new Promise(res => setTimeout(res, ms));
     import { dev } from "$app/environment"; 
-    //import FusionCharts from 'fusioncharts';
-    //import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
+    // Include the core fusioncharts file from core  -
+    import FusionCharts from 'fusioncharts';
+    import Charts from 'fusioncharts/fusioncharts.charts';
+    import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion';
+    import SvelteFC, { fcRoot } from 'svelte-fusioncharts';
 
     let API = "/api/v2/graficoLMMG";
     let graph = [];
@@ -24,6 +29,7 @@
     let partir65 = [];
     let result = "";
     let resultStatus = "";
+    let myChart = [];
 
     onMount(async () =>{
       getGraph()
@@ -63,81 +69,76 @@
             }
           
     }
-    async function loadChart(){  //,dataIndex
-// Importar la biblioteca de FusionCharts aquí
-const FusionCharts = await import('fusioncharts');
-const Temas = await import('fusioncharts/themes/fusioncharts.theme.fusion');
-// Create a JSON object to store the chart configurations
-const dataSource = {
-  chart: {
-    caption: "Evolución Andalucía",
-    subcaption: "1980-1982",
-    xaxisname: "Provincia-Año",
-    yaxisname: "Número de personas",
-    formatnumberscale: "10",
-    plottooltext:
-      "<b>$dataValue</b> personas en <b>$seriesName</b> en $label",
-    theme: "fusion",
-    drawcrossline: "1"
-  },
-  categories: [
-    {
-      category: provincia_period
-    }
-  ],
-  dataset: [
-    {
-      seriesname: "Total población",
-      data: poblaciontotal
+    async function loadChart() {
+      // Always set FusionCharts as the first parameter
+   fcRoot(FusionCharts, Charts, FusionTheme);
+  // Create a JSON object to store the chart configurations
+  const dataSource = {
+    chart: {
+      caption: "Evolución Andalucía",
+      subcaption: "1980-1982",
+      xaxisname: "Provincia-Año",
+      yaxisname: "Número de personas",
+      formatnumberscale: "10",
+      plottooltext:
+        "<b>$dataValue</b> personas en <b>$seriesName</b> en $label",
+      theme: "fusion",
+      drawcrossline: "1"
     },
-    {
-      seriesname: "Hombres",
-      data: hombres
-    },
-    {
-      seriesname: "Mujeres",
-      data: mujeres
-    },
-    {
-      seriesname: "Menor de 16",
-      data: menor16
-    },
-    {
-      seriesname: "De 16 a 64",
-      data: de16a64
-    },
-    {
-      seriesname: "A partir de 65",
-      data: partir65
-    }
-  ]
-};
+    categories: [
+      {
+        category: provincia_period
+      }
+    ],
+    dataset: [
+      {
+        seriesname: "Total población",
+        data: poblaciontotal
+      },
+      {
+        seriesname: "Hombres",
+        data: hombres
+      },
+      {
+        seriesname: "Mujeres",
+        data: mujeres
+      },
+      {
+        seriesname: "Menor de 16",
+        data: menor16
+      },
+      {
+        seriesname: "De 16 a 64",
+        data: de16a64
+      },
+      {
+        seriesname: "A partir de 65",
+        data: partir65
+      }
+    ]
+  };
 
-FusionCharts.ready(function() {
-  var myChart = new FusionCharts({
+
+  const myChart = new FusionCharts({
     type: "mscolumn2d",
     renderAt: "chart-container",
     width: "100%",
     height: "1000%",
     dataFormat: "json",
     dataSource
-  }).render();
-  console.log("CARGA");
-});
+  });
+  myChart.render();
+}
 
-
-
-
-        
-    }
     
 </script>
-
+<SvelteFC {...myChart} />
 <main>
   <h1>Grafico FusionCharts</h1>
 <figure class="FusionCharts-figure">
   <body>
     <div id="chart-container">FusionCharts XT will load here!</div>
+    <script src="/graficoLMMG/index.js"></script>
     <p class="FusionCharts-description">
       Una gráfica en la que se representa en el eje X las distintas provincias de Andalucía y en el eje y
       números de personas, finalmente los colores representan tanto la característica como el año al que pertenece.
@@ -145,6 +146,7 @@ FusionCharts.ready(function() {
     </body>
 </figure>
 </main>
+
 <!--{#if resultStatus != ""}
     <p>
         Result:
