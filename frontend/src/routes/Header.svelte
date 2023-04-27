54933eaa0697471715805f82dc065c06ec172ff6
Luis Miguel Martín <luimargon3@alum.us.es>
2023-04-27 20:23:38 +0200

    API GRUPO LUIS MIGUEL

--- frontend/src/routes/Header.svelte
+++ frontend/src/routes/Header.svelte
@@ -4,6 +4,7 @@
     <a href="/evolution">Evolución</a>
     <a href="/Usos">Usos</a>
     <a href="/Integraciones">Integraciones</a>
+    <a href="/analytics">Analíticas</a>
     <a href="/graficoLMMG">GrafoLMMG</a>
     <a href="/graficoJLB">GraficoJLB</a>
     <a href="/localentities">Entidades locales</a>
